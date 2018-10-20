ca555b39eac1aaefc4103d3b827b78c4e5dc8986
Abdullah Algurain <abdullahalgurain@Abdullahs-MacBook-Air.local>
2018-10-20 10:27:33 +0300

    the problem resolved

---
+++ Udacian.py
@@ -0,0 +1,18 @@
+class Udacian:
+   
+
+   def __init__(self, name, city,enrollment,nanodegree,status):
+      self.name = name
+      self.city = city
+      self.enrollment = enrollment
+      self.nanodegree = nanodegree
+      self.status = status
+   
+
+   def print_udacian(self):
+      print ( self.name , ' is enrolled in ', self.city, 'studying',  self.nanodegree, ' with Ms. ', self.enrollment, 'he/she is', self.status  )
+
+
+u = Udacian('ahmed','dammam','Elham','fullstack','ontrack')
+
+u.print_udacian()
